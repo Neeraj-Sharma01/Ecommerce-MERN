08b9e77679d926c23ce06a5e1996a4b0947ad4a5
Neeraj-Sharma01 <ns975821@gmail.com>
2026-07-27 15:36:51 +0530

    implement LoginApi & JWT authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs"
 import User from "../models/User.js"
+import jwt from "jsonwebtoken"
 
 export const registerUser = async(req,res) => {
     try {
@@ -44,5 +45,54 @@ export const registerUser = async(req,res) => {
            res.status(500).json({
                message:error.message
            }) 
+    }
+}
+
+export const loginUser = async(req,res) => {
+    try {
+        const {email,password} = req.body
+        if (!email || !password) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Email and password are required",
+                });
+            }
+        const user = await User.findOne({email})
+        if(!user){
+           return res.status(401).json({
+                success:false,
+                message:"User not exist"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password",
+            });
+        }
+
+        const token = jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:"7d"})
+
+        res.status(200).json({
+            success:true,
+            message:"Login Successful",
+            token,
+            user:{
+                id:user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            }
+        })
+
+
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
     }
 }
