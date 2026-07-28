ce50668c5ba7e73223447bc3820ff27fb5abf4ae
Neeraj-Sharma01 <ns975821@gmail.com>
2026-07-28 14:01:41 +0530

    implement product creating API

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,36 @@
+import Product from "../models/product.js";
 export const createProduct = async (req, res) => {
-  res.json({
-    message: "Create Product API Working",
-  });
+    try {
+        console.log("Request reached createProduct");
+        console.log(req.body);
+        const{title,description,price,category,brand,stock,images} = req.body;
+        if (!title || !description || !price || !category) {
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all required fields",
+            });
+        }
+        const product = await Product.create({
+            title,
+            description,
+            price,
+            category,
+            brand,
+            stock,
+            images,
+            createdBy:req.user._id,
+        })
+
+        res.status(201).json({
+        success: true,
+        message: "Product created successfully",
+        product,
+    });
+}
+catch (error) {
+        res.status(500).json({
+        success: false,
+        message: error.message,
+    });
+    }
 };
