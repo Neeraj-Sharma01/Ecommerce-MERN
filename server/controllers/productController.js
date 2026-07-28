b462ce2f83837469e6b308dde31d3302eb771fb7
Neeraj-Sharma01 <ns975821@gmail.com>
2026-07-28 14:18:33 +0530

    implement get product by  id

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -38,6 +38,7 @@ catch (error) {
 export const getProducts = async (req,res) => {
     try {
         const products = await Product.find().sort({createdAt: -1,});
+        
         res.status(200).json({
       success: true,
       count: products.length,
@@ -48,5 +49,27 @@ export const getProducts = async (req,res) => {
       success: false,
       message: error.message,
     }); 
+    }
+}
+
+export const getProductsById = async (req,res) => {
+    try {
+            const {id} = req.params;
+            const product = await Product.findById(id);
+             if (!product) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Product not found",
+                });
+                }
+            res.status(200).json({
+                success:true,
+                product
+            })
+    } catch (error) {
+            res.status(500).json({
+                success:false,
+                message:error.message
+            })
     }
 }
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from  "express";
-import { createProduct,getProducts } from "../controllers/productController.js";
+import { createProduct,getProducts,getProductsById } from "../controllers/productController.js";
 import { protect,admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/",getProducts);
 router.post("/",protect,admin,createProduct);
+router.get("/:id",getProductsById);
 
 export default router;
