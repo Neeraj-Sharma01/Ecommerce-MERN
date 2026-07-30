e32b1967afcb14080bb33aff5d31aa7627a8e798
Neeraj-Sharma01 <ns975821@gmail.com>
2026-07-30 13:51:30 +0530

    add get order details API with ownership check

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -84,3 +84,36 @@ export const getUserOrders = async(req,res) => {
         });
     }
 }
+
+export const getOrderById = async(req,res) => {
+    try {
+        const {id} = req.parms;
+
+        const order = await Order.findById(id).populate("items.product").populate("user","name email");
+
+         if (!order) {
+            return res.status(404).json({
+                success: false,
+                message: "Order not found",
+            });
+            }
+        
+        if (order.user._id.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                success: false,
+                message: "Access denied",
+            });
+            }
+
+             res.status(200).json({
+                success: true,
+                order,
+                });
+
+    } catch (error) {
+             res.status(200).json({
+                success: true,
+                order,
+                });
+    }
+}
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createOrder, getUserOrders } from "../controllers/orderController.js";
+import { createOrder, getOrderById, getUserOrders } from "../controllers/orderController.js";
 import {protect} from "../middleware/authMiddleware.js"
 
 const router  = express.Router();
 
 router.post("/",protect,createOrder);
 router.get("/",protect,getUserOrders);
+router.get("/:id",protect,getOrderById);
 
 export default router;
