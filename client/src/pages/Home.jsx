ffa50b78b8277cba17abf1e025f030264605a035
Neeraj-Sharma01 <ns975821@gmail.com>
2026-07-31 15:28:57 +0530

    fetch and display products on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import {useEffect,useState} from 'react';
+import api from "../api/axios.js";
 
 const Home = () => {
+  const [products,setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts()
+  },[])
+
+
+  const fetchProducts = async() => {
+    try {
+      
+      const response = await api.get("/products");
+      console.log(response.data.products);
+      setProducts(response.data.products);
+    } catch (error) {
+              console.log(error);
+    } 
+  }
+
   return (
-    <div>Home</div>
+   <div>
+    <h1>All Products</h1>
+     {products.map((product) => (
+        <div key={product._id}>
+          <h3>{product.title}</h3>
+
+          <p>₹ {product.price}</p>
+
+          <p>{product.brand}</p>
+        </div>
+      ))}
+   </div>
   )
 }
 
